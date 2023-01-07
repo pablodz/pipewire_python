38d3ded15ed0d021fe2916230ba77edbbd43e1c0
Joe Stanley <engineerjoe440@yahoo.com>
2023-01-07 10:27:04 -0800

    updated to better reflect pipewire notes, update docs

--- pipewire_python/link.py
+++ pipewire_python/link.py
@@ -1,8 +1,50 @@
 """
 PIPEWIRE's Python controller (wrapper)
 
-In the next pages you'll see documentation of each Python component
-`link.py`.
+Pipewire exposes a command-line interface known as `pw-link` to support linking
+between outputs (sources) and inputs (sinks).
+
+```bash
+$> pw-link --help
+pw-link : PipeWire port and link manager.
+Generic: pw-link [options]
+  -h, --help                            Show this help
+      --version                         Show version
+  -r, --remote=NAME                     Remote daemon name
+List: pw-link [options] [out-pattern] [in-pattern]
+  -o, --output                          List output ports
+  -i, --input                           List input ports
+  -l, --links                           List links
+  -m, --monitor                         Monitor links and ports
+  -I, --id                              List IDs
+  -v, --verbose                         Verbose port properties
+Connect: pw-link [options] output input
+  -L, --linger                          Linger (default, unless -m is used)
+  -P, --passive                         Passive link
+  -p, --props=PROPS                     Properties as JSON object
+Disconnect: pw-link -d [options] output input
+            pw-link -d [options] link-id
+  -d, --disconnect                      Disconnect ports
+```
+
+
+Examples
+--------
+>>> from pipewire_python import link
+>>> inputs = link.list_inputs()
+>>> outputs = link.list_outputs()
+>>> # Connect the last output to the last input -- during testing it was found
+>>> # that Midi channel is normally listed first, so this avoids that.
+>>> source = outputs[-1]
+>>> sink = inputs[-1]
+>>> source.connect(sink)
+>>> # Fun Fact! You can connect/disconnect in either order!
+>>> sink.disconnect(source) # Tada!
+>>> # Default Input/Output links will be made with left-left and right-right
+>>> # connections; in other words, a straight stereo connection.
+>>> # It's possible to manually cross the lines, however!
+>>> source.right.connect(sink.left)
+>>> source.left.connect(sink.right)
 """
 
 from typing import List, Union
@@ -13,6 +55,17 @@ from ._utils import (
     _execute_shell_command,
 )
 
+__all__ = [
+    "PortType",
+    "Port",
+    "Input",
+    "Output",
+    "Link",
+    "list_inputs",
+    "list_outputs",
+    "list_links",
+]
+
 
 PW_LINK_COMMAND = "pw-link"
 
@@ -20,88 +73,114 @@ PW_LINK_COMMAND = "pw-link"
 class InvalidLink(ValueError):
     """Invalid link configuration."""
 
-class ChannelType(Enum):
+class PortType(Enum):
     """Pipewire Channel Type - Input or Output."""
     INPUT = 1
     OUTPUT = 2
 
 
 @dataclass
-class Channel:
+class Port:
     """
-    Pipewire Link Channel Object.
+    Pipewire Link Port Object.
 
-    Connect or disconnect Pipewire links with unique individual channels.
+    Port for an input or output in Pipewire link. This is the basic, structural
+    component for the Python wrapper of Pipewire-link. Ports may be connected by
+    links, and Inputs/Outputs consist of one or more of these Port objects
+    corresponding to left/right channels.
 
     Attributes
     ----------
     id:             int
-                    Pipewire channel identifier.
+                    Pipewire connector identifier.
     device:         str
                     Pipewire device name.
     name:           str
-                    Pipewire device channel name, (typically uses L or R).
-    channel_type:   ChannelType
+                    Pipewire device connector name, (typically uses FL or FR).
+    port_type:      PortType
+                    Designation of connector as input or output.
+    is_midi:        bool
+                    Indicator to mark that the port is a Midi connection.
     """
 
     device: str
     name: str
     id: int
-    channel_type: ChannelType
+    port_type: PortType
+    is_midi: bool = False
 
-    def _join_arguments(self, other: "Channel", message: str) -> List[str]:
+    def _join_arguments(self, other: "Port", message: str) -> List[str]:
         """Generate a list of arguments to appropriately set output, then input
         for the connection/disconnection command."""
         args = [PW_LINK_COMMAND]
-        if self.channel_type == ChannelType.INPUT:
-            if other.channel_type == ChannelType.INPUT:
+        if self.port_type == PortType.INPUT:
+            if other.port_type == PortType.INPUT:
                 raise InvalidLink(message.format("input"))
             # Valid -- Append the Output (other) First
             args.append(":".join((other.device, other.name)))
             args.append(":".join((self.device, self.name)))
         else:
-            if other.channel_type == ChannelType.OUTPUT:
+            if other.port_type == PortType.OUTPUT:
                 raise InvalidLink(message.format("output"))
             # Valid -- Append the Output (self) First
             args.append(":".join((self.device, self.name)))
             args.append(":".join((other.device, other.name)))
         return args
 
-    def connect(self, other: "Channel"):
+    def connect(self, other: "Port"):
         """Connect this channel to another channel."""
         args = self._join_arguments(
             other=other,
             message="Cannot connect an {} to another {}."
         )
-        stdout, stderr = _execute_shell_command(args)
-    
-    def disconnect(self, other: "Channel"):
+        _ = _execute_shell_command(args)
+
+    def disconnect(self, other: "Port"):
         """Disconnect this channel from another."""
         args = self._join_arguments(
             other=other,
             message="Cannot disconnect an {} from another {}."
         )
         args.append("--disconnect")
-        stdout, stderr = _execute_shell_command(args)
-
+        _ = _execute_shell_command(args)
 
 @dataclass
 class Input:
     """
     Pipewire Input Object.
 
-    Grouping of left and right channels for a Pipewire link input.
+    Grouping of left and right channel ports for a Pipewire link input.
+
+    Examples
+    --------
+    >>> from pipewire_python import link
+    >>> inputs = link.list_inputs() # List the inputs on the system.
+    >>> # Inputs can also manually built
+    >>> my_input = link.Input(
+    ...     left = link.Port(
+    ...         id=123,
+    ...         device="alsa.my.device",
+    ...         name="FL",
+    ...         port_type=link.PortType.INPUT
+    ...     ),
+    ...     right = link.Port(
+    ...         id=321,
+    ...         device="alsa.my.device",
+    ...         name="FR",
+    ...         port_type=link.PortType.INPUT
+    ...     )
+    ... )
 
     Attributes
     ----------
-    left:   Channel
-            Left (or mono) channel.
-    right:  Channel
-            Right channel.
+    left:   Port
+            Left (or mono) channel port.
+    right:  Port
+            Right channel port.
     """
 
-    left: Channel
-    right: Channel
+    left: Port
+    right: Port
 
     def connect(self, output_device: "Output") -> Union["Link", None]:
         """Connect this input to an output."""
@@ -128,18 +207,38 @@ class Output:
     """
     Pipewire Output Object.
 
-    Grouping of left and right channels for a Pipewire link output.
+    Grouping of left and right channel ports for a Pipewire link output.
+
+    Examples
+    --------
+    >>> from pipewire_python import link
+    >>> outputs = link.list_outputs() # List the outputs on the system.
+    >>> # Outputs can also manually built
+    >>> my_output = link.Output(
+    ...     left = link.Port(
+    ...         id=123,
+    ...         device="alsa.my.device",
+    ...         name="FL",
+    ...         port_type=link.PortType.OUTPUT
+    ...     ),
+    ...     right = link.Port(
+    ...         id=321,
+    ...         device="alsa.my.device",
+    ...         name="FR",
+    ...         port_type=link.PortType.OUTPUT
+    ...     )
+    ... )
 
     Attributes
     ----------
-    left:   Channel
-            Left (or mono) channel.
-    right:  Channel
-            Right channel.
+    left:   Port
+            Left (or mono) channel port.
+    right:  Port
+            Right channel port.
     """
 
-    left: Channel
-    right: Channel
+    left: Port
+    right: Port
 
     def connect(self, input_device: "Input") -> Union["Link", None]:
         """Connect this input to an output."""
@@ -196,16 +295,31 @@ def _split_id_from_name(command) -> List[List[str]]:
 
 
 def list_inputs() -> List[Input]:
-    """List the Inputs."""
+    """
+    List the Inputs Available on System.
+
+    This will identify the available inputs on the system, and proceed with a
+    best-effort Input port grouping between left-and-right ports.
+
+    ```bash
+    #!/bin/bash
+    # Get inputs from output of:
+    pw-link --input --id
+    ```
+
+    Returns
+    -------
+    list[Input]:    List of the identified inputs.
+    """
     channels = []
     for channel_id, channel_data in _split_id_from_name("--input"):
         device, name = channel_data.split(':', maxsplit=1)
         channels.append(
-            Channel(
+            Port(
                 id=channel_id,
                 device=device,
                 name=name,
-                channel_type=ChannelType.INPUT
+                port_type=PortType.INPUT
             )
         )
     i = 0
@@ -238,16 +352,31 @@ def list_inputs() -> List[Input]:
 
 
 def list_outputs() -> List[Output]:
-    """List the Outputs."""
+    """
+    List the Outputs Available on System.
+
+    This will identify the available outputs on the system, and proceed with a
+    best-effort Output port grouping between left-and-right ports.
+
+    ```bash
+    #!/bin/bash
+    # Get outputs from output of:
+    pw-link --output --id
+    ```
+
+    Returns
+    -------
+    list[Output]:    List of the identified outputs.
+    """
     channels = []
     for channel_id, channel_data in _split_id_from_name("--output"):
         device, name = channel_data.split(':', maxsplit=1)
         channels.append(
-            Channel(
+            Port(
                 id=channel_id,
                 device=device,
                 name=name,
-                channel_type=ChannelType.OUTPUT
+                port_type=PortType.OUTPUT
             )
         )
     i = 0
@@ -280,6 +409,21 @@ def list_outputs() -> List[Output]:
 
 
 def list_links() -> List[Link]:
-    """List the Links."""
+    """
+    List the Links Available on System.
+
+    This will identify the present Pipewire links on the system, and proceed
+    with a best-effort Link port grouping between left-and-right ports.
+
+    ```bash
+    #!/bin/bash
+    # Get links from output of:
+    pw-link --links --id
+    ```
+
+    Returns
+    -------
+    list[Link]:    List of the identified links.
+    """
     # TODO
     stdout, _ = _execute_shell_command([PW_LINK_COMMAND, "--links", "--id"])
