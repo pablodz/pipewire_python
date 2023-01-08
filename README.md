550d8a43a86089eb06cf3f077bb77798833de9ca
Joe Stanley <engineerjoe440@yahoo.com>
2023-01-08 10:53:14 -0800

    updated according to review suggestions, added documentation to __init__ and README

--- README.md
+++ README.md
@@ -97,6 +97,31 @@ audio_controller.get_list_interfaces(
 )
 ```
 
+#### LINK PORTS
+### Linking Ports
+
+```python
+from pipewire_python import link
+
+inputs = link.list_inputs()
+outputs = link.list_outputs()
+
+# Connect the last output to the last input -- during testing it was found that
+# Midi channel is normally listed first, so this avoids that.
+source = outputs[-1]
+sink = inputs[-1]
+source.connect(sink)
+
+# Fun Fact! You can connect/disconnect in either order!
+sink.disconnect(source) # Tada!
+
+# Default Input/Output links will be made with left-left and right-right
+# connections; in other words, a straight stereo connection.
+# It's possible to manually cross the lines, however!
+source.right.connect(sink.left)
+source.left.connect(sink.right)
+```
+
 
 ## Documentation
 
--- pipewire_python/__init__.py
+++ pipewire_python/__init__.py
@@ -65,9 +65,33 @@ audio_controller.record(audio_filename='docs/5sec_record.wav',
                         verbose=True)
 
 ```
+
+### Linking Ports
+
+```python
+from pipewire_python import link
+
+inputs = link.list_inputs()
+outputs = link.list_outputs()
+
+# Connect the last output to the last input -- during testing it was found that
+# Midi channel is normally listed first, so this avoids that.
+source = outputs[-1]
+sink = inputs[-1]
+source.connect(sink)
+
+# Fun Fact! You can connect/disconnect in either order!
+sink.disconnect(source) # Tada!
+
+# Default Input/Output links will be made with left-left and right-right
+# connections; in other words, a straight stereo connection.
+# It's possible to manually cross the lines, however!
+source.right.connect(sink.left)
+source.left.connect(sink.right)
+```
 """
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 import sys
 
